6d3f99f5cb36a979cb002d934f6a8b223c84f846
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-27 16:30:03 -0300

    fix: dotenv config

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,15 +1,16 @@
-import { z } from "zod";
+import { z } from "zod" 
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().url(),
-});
+    VITE_API_URL: z.string().url()
+})
 
-export const _env = envSchema.safeParse(process.env);
+export const _env = envSchema.safeParse(import.meta.env)
+
 
 if (_env.success === false) {
-  console.error("❌ Invalid environment variables", _env.error.format());
-
-  throw new Error("Invalid environment variables");
-}
-
-export const env = _env.data;
+    console.error("❌ Invalid environment variables", _env.error.format());
+  
+    throw new Error("Invalid environment variables.");
+  }
+  
+  export const env = _env.data;
