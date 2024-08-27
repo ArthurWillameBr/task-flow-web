6cc68776b4fef7f132ea7a42033a124950602714
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-27 13:51:44 -0300

    chore: ensure environment variables are set correctly

---
+++ src/lib/env.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  VITE_API_URL: z.string().url(),
+});
+
+export const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("❌ Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
