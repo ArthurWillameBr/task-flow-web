138e744609cc388a52d9af0e4703177d77608a49
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-27 16:31:09 -0300

    feat: add auth context

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,68 @@
+import { UserProps } from "@/@types/user";
+import { api } from "@/lib/axios";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+interface AuthContextProps {
+  user: UserProps | null;
+  signIn: (user: UserProps) => Promise<void>;
+  signOut: () => void;
+  isAuthenticated: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  useEffect(() => {
+    const loadingStorageData = async () => {
+      const storageUser = localStorage.getItem("@Auth:user");
+      const storageToken = localStorage.getItem("@Auth:token");
+
+      if (storageUser && storageToken) {
+        setUser(JSON.parse(storageUser));
+        api.defaults.headers.common["Authorization"] = `Bearer ${storageToken}`;
+      } else {
+        setUser(null);
+      }
+    };
+    loadingStorageData();
+  }, []);
+
+  const signIn = async ({ email, password }: UserProps) => {
+    const response = await api.post("/sessions", {
+      email,
+      password,
+    });
+
+    if (response.data.error) {
+      console.log(response.data.error);
+    } else {
+      setUser(response.data.user);
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.token}`;
+
+      localStorage.setItem("@Auth:user", JSON.stringify(response.data.user));
+      localStorage.setItem("@Auth:token", response.data.token);
+    }
+  };
+
+  const signOut = () => {
+    setUser(null);
+    localStorage.removeItem("@Auth:user");
+    localStorage.removeItem("@Auth:token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ signIn, signOut, user, isAuthenticated: !!user }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
