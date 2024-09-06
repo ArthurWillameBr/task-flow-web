34897e3458c45541a585633be49f67d559e01606
arthurwillame5 <arthurwillame2005@gmail.com>
2024-09-05 22:13:19 -0300

    feat: add TaskTableSkeleton componente

---
+++ src/components/task-table-skeleton.tsx
@@ -0,0 +1,42 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+export default function TaskTableSkeleton() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Título</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Prioridade</TableHead>
+          <TableHead>Ações</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {[...Array(5)].map((_, index) => (
+          <TableRow key={index}>
+            <TableCell>
+              <Skeleton className="h-6 w-[188px]" />
+            </TableCell>
+            <TableCell>
+              <Skeleton className="h-8 w-24" />
+            </TableCell>
+            <TableCell>
+              <Skeleton className="h-8 w-24" />
+            </TableCell>
+            <TableCell>
+              <Skeleton className="h-8 w-8 rounded-full" />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
